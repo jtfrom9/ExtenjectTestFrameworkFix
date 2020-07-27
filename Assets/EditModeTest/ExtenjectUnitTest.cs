d7fab3e7fed85cdc247373e47f402c1e5fc1bb4b
jtfrom9 <jun.tachikawa@gmail.com>
2020-07-28 03:48:52 +0900

    make sure TestFramework works

--- Assets/EditModeTest/ExtenjectUnitTest.cs
+++ Assets/EditModeTest/ExtenjectUnitTest.cs
@@ -4,9 +4,19 @@ using NUnit.Framework;
 [TestFixture]
 public class ExtenjectUnitTest : ZenjectUnitTestFixture
 {
+    [Inject]
+    IFoo foo;
+
+    [SetUp]
+    public void CommonInstall()
+    {
+        Container.Bind<IFoo>().To<Foo>().AsSingle();
+        Container.Inject(this);
+    }
+
     [Test]
     public void RunTest1()
     {
-        // TODO
+        Assert.That(foo.Foo(), Is.EqualTo(10));
     }
 }
---
+++ Assets/EditModeTest/Target.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFoo
+{
+    int Foo();
+}
+
+public class Foo : IFoo
+{
+    int IFoo.Foo()
+    {
+        return 10;
+    }
+}
